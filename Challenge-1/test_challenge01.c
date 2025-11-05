773d48525532ea24be55f52e1cf19f49933e2500
edusan19 <157134130+edusan19@users.noreply.github.com>
2025-11-05 12:22:48 +0100

    feat: return en starter.c + test_suma

---
+++ Challenge-1/test_challenge01.c
@@ -0,0 +1,11 @@
+int suma(int a, int b) {
+    return a + b;
+}
+
+// testeo de la función suma
+#include <assert.h>
+void test_suma() {
+    assert(suma(2, 3) == 5);
+    assert(suma(-1, 1) == 0);
+    assert(suma(-2, -3) == -5);
+}
